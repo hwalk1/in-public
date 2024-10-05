ecf504638ee33918182b55bbcb0461fa3dd4506f
hwalk1 <haydenjwalker1@gmail.com>
2024-10-05 19:15:59 +1000

    Place Holder Navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
 import { Button } from "@nextui-org/button";
+import NavBar from "@/components/NavBar";
+import { NextUIProvider } from "@nextui-org/react";
 
 export default function Home() {
-  return <Button>Press me</Button>;
+  return (
+    <>
+      <NextUIProvider>
+        <NavBar />
+        <Button>Press me</Button>;<h3>Test</h3>
+      </NextUIProvider>
+    </>
+  );
 }
