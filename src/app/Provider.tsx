fc515f222ec0bbfaee2f14e227ff002b7810a43f
hwalk1 <haydenjwalker1@gmail.com>
2024-10-07 17:02:00 +1100

    Update NextUI Provider

---
+++ src/app/Provider.tsx
@@ -0,0 +1,6 @@
+import { NextUIProvider } from "@nextui-org/react";
+
+export default function App(children: React.ReactNode) {
+  // 2. Wrap NextUIProvider at the root of your app
+  return <NextUIProvider>{children}</NextUIProvider>;
+}
