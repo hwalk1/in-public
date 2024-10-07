30c156487ca581541e72cc6ad026e589757fd351
hwalk1 <haydenjwalker1@gmail.com>
2024-10-07 17:06:40 +1100

    Provider Cleanup

--- src/app/Provider.tsx
+++ src/app/Provider.tsx
@@ -1,6 +1,10 @@
 import { NextUIProvider } from "@nextui-org/react";
 
-export default function App(children: React.ReactNode) {
+export default function App({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   // 2. Wrap NextUIProvider at the root of your app
   return <NextUIProvider>{children}</NextUIProvider>;
 }
