77efb3f19da771b7893448d69bb22ef6adbfe4a6
hwalk1 <haydenjwalker1@gmail.com>
2024-10-08 21:34:43 +1100

    Hero Page Init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { Button } from "@nextui-org/button";
 import NavBar from "@/components/NavBar";
+import { Hero } from "@/components/Hero";
 
 export default function Home() {
   return (
     <>
       <NavBar />
-      <Button>Press me</Button>;<h3>Test</h3>
+      <Hero />
+      <Button color="primary">Press me</Button>;<h3>Test</h3>
     </>
   );
 }
