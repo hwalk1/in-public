e265045fdded29ae2a024192e6778f0828267a3c
hwalk1 <haydenjwalker1@gmail.com>
2024-10-12 20:23:03 +1100

    Demo init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import NavBar from "@/components/NavBar";
 import { Hero } from "@/components/Hero";
+import Demo from "@/components/Demo";
 
 export default function Home() {
   return (
     <>
       <NavBar />
       <Hero />
+      <Demo />
     </>
   );
 }
---
+++ src/components/Demo.tsx
@@ -0,0 +1,5 @@
+const Demo = () => {
+  return <div>Demotest</div>;
+};
+
+export default Demo;
