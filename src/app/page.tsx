c413cf61436316268793cff54472579306316251
hwalk1 <haydenjwalker1@gmail.com>
2024-10-12 20:25:36 +1100

    Revert "Demo init"
    This reverts commit e265045fdded29ae2a024192e6778f0828267a3c.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 import NavBar from "@/components/NavBar";
 import { Hero } from "@/components/Hero";
-import Demo from "@/components/Demo";
 
 export default function Home() {
   return (
     <>
       <NavBar />
       <Hero />
-      <Demo />
     </>
   );
 }
