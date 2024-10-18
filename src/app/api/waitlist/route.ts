7d30876062bf4969de0f5cf468173c3e3f4c5603
hwalk1 <haydenjwalker1@gmail.com>
2024-10-18 20:09:32 +1100

    SupaBase API Test

--- src/app/api/waitlist/route.ts
+++ src/app/api/waitlist/route.ts
@@ -1,9 +1,25 @@
-import { NextResponse } from "next/server";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { createClient } from "@supabase/supabase-js";
 
-export async function POST(request: Request) {
-  const data = await request.json();
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  );
+  console.log({ supabase });
+  const { error } = await supabase
+    .from("countires")
+    .insert({ id: 1, name: "Denmark" });
 
-  return NextResponse.json({
-    data,
-  });
+  const { email, username } = req.body;
+
+  try {
+    await handleFormInputAsync({ name, message });
+    res.redirect(307, "/");
+  } catch (err) {
+    res.status(500).send({ error: "Failed to fetch data", err });
+  }
 }
