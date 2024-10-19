6545dedd7153273c6aea0675c8634974d0e9ea13
hwalk1 <haydenjwalker1@gmail.com>
2024-10-19 12:51:30 +1100

    POST Ressponse

--- src/app/api/waitlist/route.ts
+++ src/app/api/waitlist/route.ts
@@ -1,25 +1,52 @@
-import type { NextApiRequest, NextApiResponse } from "next";
 import { createClient } from "@supabase/supabase-js";
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
+export async function POST() {
   const supabase = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   );
-  console.log({ supabase });
-  const { error } = await supabase
-    .from("countires")
-    .insert({ id: 1, name: "Denmark" });
 
-  const { email, username } = req.body;
-
-  try {
-    await handleFormInputAsync({ name, message });
-    res.redirect(307, "/");
-  } catch (err) {
-    res.status(500).send({ error: "Failed to fetch data", err });
-  }
+  console.log(supabase);
+  return Response.json({ supabase });
 }
+
+// export async function GET() {
+//   const res = await fetch("https://data.mongodb-api.com/...", {
+//     headers: {
+//       "Content-Type": "application/json",
+//       "API-Key": process.env.DATA_API_KEY,
+//     },
+//   });
+//   const data = await res.json();
+
+//   return Response.json({ data });
+// }
+
+// export default async function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse
+// ) {
+// const supabase = createClient(
+//   process.env.NEXT_PUBLIC_SUPABASE_URL,
+//   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+// );
+// if (req.method === "POST") {
+//   console.log("hello");
+// } else {
+// Handle any other HTTP method
+// }
+
+// console.log({ supabase });
+// const { error } = await supabase
+//   .from("countires")
+//   .insert({ id: 1, name: "Denmark" });
+
+// const { email, username } = req.body;
+
+// try {
+//   await handleFormInputAsync({ name, message });
+//   res.redirect(307, "/");
+// } catch (err) {
+//   res.status(500).send({ error: "Failed to fetch data", err });
+// }
+// }
