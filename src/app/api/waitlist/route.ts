8b3dbda985b5e074cb6f0fc12b8eeba499c54463
hwalk1 <haydenjwalker1@gmail.com>
2024-10-25 16:49:29 +1100

    API 200 Response

--- src/app/api/waitlist/route.ts
+++ src/app/api/waitlist/route.ts
@@ -11,47 +11,6 @@ export async function POST(request: Request) {
   const { data } = await supabase
     .from("sign-up")
     .insert({ username: res.username, email: res.email });
-
+  console.log(Response.json);
   return Response.json(data);
 }
-
-// export async function GET() {
-//   const res = await fetch("https://data.mongodb-api.com/...", {
-//     headers: {
-//       "Content-Type": "application/json",
-//       "API-Key": process.env.DATA_API_KEY,
-//     },
-//   });
-//   const data = await res.json();
-
-//   return Response.json({ data });
-// }
-
-// export default async function handler(
-//   req: NextApiRequest,
-//   res: NextApiResponse
-// ) {
-// const supabase = createClient(
-//   process.env.NEXT_PUBLIC_SUPABASE_URL,
-//   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-// );
-// if (req.method === "POST") {
-//   console.log("hello");
-// } else {
-// Handle any other HTTP method
-// }
-
-// console.log({ supabase });
-// const { error } = await supabase
-//   .from("countires")
-//   .insert({ id: 1, name: "Denmark" });
-
-// const { email, username } = req.body;
-
-// try {
-//   await handleFormInputAsync({ name, message });
-//   res.redirect(307, "/");
-// } catch (err) {
-//   res.status(500).send({ error: "Failed to fetch data", err });
-// }
-// }
